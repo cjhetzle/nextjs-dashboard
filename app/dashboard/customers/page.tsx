ed637a73902baef9db1aca92d9e2b8c092b3b550
Cameron Hetzler <cjhetz@gmail.com>
2023-12-12 02:08:25 -0500

    Finished login

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,23 @@
-export default function Page() {
-    return <p>Customers Page</p>;
+import { fetchCustomersPages, fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+import { Suspense } from "react";
+
+export default async function Page({
+    searchParams,
+}: {
+    searchParams?: {
+        query?: string;
+        page?: string;
+    };
+}) {
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
+    const totalPages = await fetchCustomersPages(query);
+    const customers = await fetchFilteredCustomers(query);
+
+    return (
+        <Suspense key={query + currentPage}>
+            <CustomersTable customers={customers} />
+        </Suspense>
+    );
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -232,6 +232,24 @@ export async function fetchFilteredCustomers(query: string) {
   }
 }
 
+export async function fetchCustomersPages(query: string) {
+  noStore();
+  try {
+    const count = await sql`SELECT COUNT(*)
+    FROM customers
+    WHERE
+      customers.name ILIKE ${`%${query}%`} OR
+      customers.email ILIKE ${`%${query}%`}
+  `;
+
+    const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
+    return totalPages;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch total number of invoices.');
+  }
+}
+
 export async function getUser(email: string) {
   noStore();
   try {
